0f69119127dcb41090353099db63c1fbf38d2c19
Sam Sarsten <sam.sarsten@gmail.com>
2022-08-24 21:39:10 -0700

    Set up game function (pre-testing)

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-
+// Global variables
 const SELECTIONS = ["Rock", "Paper", "Scissors"]
+const messageWin = "You won this round!!!";
+const messageLose = "You lost this round...";
+const messageTie = "It's a tie!"
+
 // Get the computer's choice
 function getComputerChoice() {
     let cpuChoice = Math.floor(Math.random() * 3);
     return SELECTIONS[cpuChoice]
 }
 
-
-
-
-
 function playRound(playerSelection, computerSelection) {
-    const messageWin = "You won this round!!!";
-    const messageLose = "You lost this round...";
-    const messageTie = "It's a tie!"
     let message = messageLose;
 
     if (playerSelection === computerSelection) {
@@ -25,6 +22,33 @@ function playRound(playerSelection, computerSelection) {
     return message;
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
+function game() {
+    let counter = [0,0,0];
+    let result = "";
+    let playerSelection = 0;
+    let computerSelection = 0;
+
+    while (playerSelection < 1 && playerSelection > 3) {
+        playerSelection = prompt("What's your choice?", "1 = Rock, 2 = Scissors, 3 = Paper");
+    }
+    playerSelection = SELECTIONS[playerSelection - 1];
+    
+    computerSelection = getComputerChoice();
+
+    for (let i = 0; i < 5; i++) {
+        result = playRound(playerSelection, computerSelection);
+
+        if (result === messageWin) {
+            counter[0]++;
+        } else if (result === messageLose) {
+            counter[1]++;
+        } else {
+            counter[2]++;
+        }
+    }
+
+    alert("You won: " + counter[0] + ", You lost: " + counter[1] + ", You tied: " + counter[2] + ". Refresh to play again!")
+}
+
+
 console.log(playRound(playerSelection, computerSelection));
