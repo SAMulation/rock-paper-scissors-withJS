acb9fc924cc3535d85c5b37bb1d92eac4915eb72
Sam Sarsten <sam.sarsten@gmail.com>
2022-09-01 12:54:37 -0700

    Cleaned up code
    Finished resetGame()
    
    Changed header
    
    Updated css to match html changes

--- script.js
+++ script.js
@@ -3,31 +3,61 @@ const SELECTIONS = ["rock", "paper", "scissors"]
 const messageWin = "You won this round!!!";
 const messageLose = "You lost this round...";
 const messageTie = "It's a tie!"
-let roundResults = document.getElementById('round');
-let gameResults = document.getElementById('game');
+const roundResults = document.getElementById('round');
+const gameResults = document.getElementById('end');
 let counter = [0,0,0];
+let gameOver = false;
 
 // Hide the #round and #game results
 roundResults.classList.toggle('inactive');
 gameResults.classList.toggle('inactive');
 
+// class 
 // buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('#actionButtons button');
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
 
   // and for each one we add a 'click' listener
   button.addEventListener('click', () => {
-    console.log("Player choice: " + button.id)
-    playRound(button.id, getComputerChoice());
+    console.log("Player choice: " + button.dataset.action)
+    if (!gameOver) {
+        playRound(button.dataset.action, getComputerChoice());
+    }
   });
 });
+
+const endButton = document.querySelector('.endButton');
+endButton.addEventListener('click', () => {
+    resetGame();
+});
+
 
 // Get the computer's choice
 function getComputerChoice() {
     let cpuChoice = Math.floor(Math.random() * 3);
     return SELECTIONS[cpuChoice]
+}
+
+function resetGame() {
+    counter = [0,0,0];
+    let spans = document.querySelectorAll('span');
+    let selectedElem = "";
+    for (let i = 0; i < 3; i++) {
+        // if (i === 0) {
+        //     selectedElem = "#won span";
+        // } else if (i === 1) {
+        //     selectedElem = "#lost span";
+        // } else {
+        //     selectedElem = "#tied span"
+        // }
+        spans[i].textContent = counter[i];
+    }
+    gameOver = false;
+    // Hide the #round and #game results
+    roundResults.classList.toggle('inactive');
+    gameResults.classList.toggle('inactive');
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -70,7 +100,13 @@ function playRound(playerSelection, computerSelection) {
     }
     console.log(counter);
     console.log(message);
-    return message;
+    //return message;
+
+    if (counter[0] === 5 || counter[1] === 5 || counter [2] === 5) {
+        gameOver = true;
+        gameResults.classList.toggle('inactive');
+        
+    }
 }
 
 //function game() {
